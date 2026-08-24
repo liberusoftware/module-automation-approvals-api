575a6c157c86fda83d120852f4ed9769780de453
Liberu Automation <opensource@liberu.co.uk>
2026-08-24 16:24:19 +0000

    Publish Automation package

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Liberu\Modules\Automation\Approvals\Api\Http\Controllers\ApprovalsResourceController;
 
-Route::middleware(['api', 'auth:sanctum'])->prefix('api/v1/automation/approvals')->group(function (): void {
+Route::middleware(['api', 'auth:sanctum', 'throttle:60,1'])->prefix('api/v1/automation/approvals')->group(function (): void {
     Route::get('/', [ApprovalsResourceController::class, 'index']);
     Route::post('/', [ApprovalsResourceController::class, 'store']);
     Route::get('/{id}', [ApprovalsResourceController::class, 'show']);
